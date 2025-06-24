cde901e1cc41f985b3cbb4022db3aed312bd461c
faith-ogun <faithogun12@gmail.com>
2025-06-24 15:43:23 +0100

    updating paths.

--- streamlit_app/home.py
+++ streamlit_app/home.py
@@ -17,17 +17,17 @@ st.markdown("### 🚀 Available Modules")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image("streamlit_app/assets/gene_biomarker.png", width=300)
+    st.image("streamlit_app/assets/gene_biomarker.png", width=250)
     st.markdown("#### Gene Biomarker Dashboard")
     st.write("Visualize multi-omics data to uncover protein-coding genes associated with HGSOC.")
 
 with col2:
-    st.image("streamlit_app/assets/synthetic_lethality.png", width=300)
+    st.image("streamlit_app/assets/synthetic_lethality.png", width=250)
     st.markdown("#### Explore Synthetic Lethality")
     st.write("Identify potential drug targets that exhibit synthetic lethality with cancer-specific mutations.")
 
 with col3:
-    st.image("streamlit_app/assets/responder-non-responder.png", width=300)
+    st.image("streamlit_app/assets/responder-non-responder.png", width=250)
     st.markdown("#### Drug Response Prediction")
     st.write("Visualize machine models predictictions on how TCGA PanCancer patients will respond to different cancer therapies.")
 
